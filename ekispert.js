d875f722ccaaf493b870af96fd404adad461df60
hmaruyama <hmaruyama@users.noreply.github.com>
2018-06-13 15:22:45 +0900

    アプリ固有のIDを使用しない方法を採用しました。

--- ekispert.js
+++ ekispert.js
@@ -1,17 +1,7 @@
 jQuery(function($) {
   "use strict";
 
-
-  kintone.events.on(['app.record.detail.show'], function(event) {
-    // 隠しパラメータ項目のDOMを削除
-    $(".label-5125682").remove();
-  });
-
   kintone.events.on(['app.record.edit.change.明細', 'app.record.create.change.明細', 'app.record.create.show', 'app.record.edit.show'], function(event) {
-
-    // 隠しパラメータ項目のDOMを削除
-    $(".label-5125682").remove();
-    $(".field-5125682").remove();
 
     var table = event.record['明細'].value;
     for (var i = 0; i < table.length; i++) {
