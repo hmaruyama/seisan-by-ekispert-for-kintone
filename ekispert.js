2caed1388e6843c9bdc6880060d7d7fc400ba4d9
HikaruMaruyama <hmaruyama@users.noreply.github.com>
2018-07-04 11:07:54 +0900

    ネストを解消しました。

--- ekispert.js
+++ ekispert.js
@@ -30,91 +30,111 @@ jQuery(function($) {
     var changeRow = event.changes.row;
     var date = changeRow.value['日付'].value.replace(/-/g, '');
     if(!changeRow.value['隠しパラメータ'].value) { return; }
-    // if(changeRow.value['隠しパラメータ'].value) {
+    var condition;
+    var depStationPart;
+    var arrStationPart;
+    var courseResult;
+    var inputOptions = {};
+    var selectRoutes = {};
+    var depStation = {};
+    var arrStation = {};
 
-      var condition;
-      var depStationPart;
-      var arrStationPart;
-      var courseResult;
-      var inputOptions = {};
-      var selectRoutes = {};
-      var depStation = {};
-      var arrStation = {};
+    swal({
+      title: "駅を入力してください",
+      html:'<div id="condition"></div>出発<div id="input-dep-station"></div>到着<div id="input-arr-station"></div><div id="course-result" style="display:none;">',
+      onOpen: function () {
+        // 探索条件
+        condition = new expGuiCondition(document.getElementById("condition"));
+        condition.setConfigure("ssl", true);
+        condition.dispCondition();
 
-      swal({
-        title: "駅を入力してください",
-        html:'<div id="condition"></div>出発<div id="input-dep-station"></div>到着<div id="input-arr-station"></div><div id="course-result" style="display:none;">',
-        onOpen: function () {
-          // 探索条件
-          condition = new expGuiCondition(document.getElementById("condition"));
-          condition.setConfigure("ssl", true);
-          condition.dispCondition();
+        // 出発駅
+        depStationPart = new expGuiStation(document.getElementById("input-dep-station"));
+        depStationPart.setConfigure("ssl", true);
+        depStationPart.dispStation();
 
-          // 出発駅
-          depStationPart = new expGuiStation(document.getElementById("input-dep-station"));
-          depStationPart.setConfigure("ssl", true);
-          depStationPart.dispStation();
+        // 到着駅
+        arrStationPart = new expGuiStation(document.getElementById("input-arr-station"));
+        arrStationPart.setConfigure("ssl", true);
+        arrStationPart.dispStation();
 
-          // 到着駅
-          arrStationPart = new expGuiStation(document.getElementById("input-arr-station"));
-          arrStationPart.setConfigure("ssl", true);
-          arrStationPart.dispStation();
-
-          // 探索結果
-          courseResult = new expGuiCourse(document.getElementById("course-result"));
-          courseResult.setConfigure("ssl", true);
-        },
-        preConfirm: function () {
-          return new Promise(function (resolve) {
-            depStation.code = depStationPart.getStationCode();
-            arrStation.code = arrStationPart.getStationCode();
-            if (!depStation.code || !arrStation.code) {
-              swal.showValidationError("駅を選択してください。");
+        // 探索結果
+        courseResult = new expGuiCourse(document.getElementById("course-result"));
+        courseResult.setConfigure("ssl", true);
+      },
+      preConfirm: function () {
+        return new Promise(function (resolve) {
+          depStation.code = depStationPart.getStationCode();
+          arrStation.code = arrStationPart.getStationCode();
+          if (!depStation.code || !arrStation.code) {
+            swal.showValidationError("駅を選択してください。");
+            resolve();
+            return;
+          }
+          var searchObject = courseResult.createSearchInterface();
+          searchObject.setAnswerCount(condition.getAnswerCount());
+          searchObject.setDate(date);
+          searchObject.setSort(condition.getSortType());
+          searchObject.setSearchType('plain');
+          searchObject.setConditionDetail(condition.getConditionDetail());
+          searchObject.setViaList(depStation.code + ':' + arrStation.code);
+          if (kintone.app.record.get().record['通勤経路'].value) { // 通勤経路項目に値が入っていれば定期割り当てを行う
+            searchObject.setAssignDetailRoute(kintone.app.record.get().record['通勤経路'].value);
+          }
+          courseResult.search(searchObject, function(isSuccess) {
+            if(!isSuccess){
+              swal.showValidationError("探索結果が取得できませんでした");
               resolve();
               return;
             }
-            var searchObject = courseResult.createSearchInterface();
-            searchObject.setAnswerCount(condition.getAnswerCount());
-            searchObject.setDate(date);
-            searchObject.setSort(condition.getSortType());
-            searchObject.setSearchType('plain');
-            searchObject.setConditionDetail(condition.getConditionDetail());
-            searchObject.setViaList(depStation.code + ':' + arrStation.code);
-            if (kintone.app.record.get().record['通勤経路'].value) { // 通勤経路項目に値が入っていれば定期割り当てを行う
-              searchObject.setAssignDetailRoute(kintone.app.record.get().record['通勤経路'].value);
+            for (var i = 1; i <= courseResult.getResultCount(); i++) {
+              courseResult.changeCourse(i);
+              var onewayPrice = courseResult.getPrice(courseResult.PRICE_ONEWAY);
+              var pointList = courseResult.getPointList().split(',');
+              var lineList = courseResult.getLineList().split(',');
+              var routeStr = "";
+              for (var j = 0; j < pointList.length; j++) {
+                if (lineList[j]) {
+                  routeStr += pointList[j] + " - [" + lineList[j] + "] - "
+                } else {
+                  routeStr += pointList[j]
+                }
+              }
+              selectRoutes[String(i)] = {
+                route: routeStr,
+                price: onewayPrice
+              }
+              inputOptions[String(i)] = routeStr + " 片道" +  onewayPrice + "円";
             }
-            courseResult.search(searchObject, function(isSuccess) {
-              if(!isSuccess){
-                swal.showValidationError("探索結果が取得できませんでした");
-                resolve();
-                return;
-              }
-              for (var i = 1; i <= courseResult.getResultCount(); i++) {
-                courseResult.changeCourse(i);
-                var onewayPrice = courseResult.getPrice(courseResult.PRICE_ONEWAY);
-                var pointList = courseResult.getPointList().split(',');
-                var lineList = courseResult.getLineList().split(',');
-                var routeStr = "";
-                for (var j = 0; j < pointList.length; j++) {
-                  if (lineList[j]) {
-                    routeStr += pointList[j] + " - [" + lineList[j] + "] - "
-                  } else {
-                    routeStr += pointList[j]
-                  }
-                }
-                selectRoutes[String(i)] = {
-                  route: routeStr,
-                  price: onewayPrice
-                }
-                inputOptions[String(i)] = routeStr + " 片道" +  onewayPrice + "円";
-              }
-              resolve();
-            });
-          })
+            resolve();
+          });
+        })
+      }
+    }).then(function (result) {
+      if (!depStation.code || !arrStation.code) {
+
+        // テーブル値の更新
+        var rec = kintone.app.record.get();
+        var tableRecord = rec.record['明細'].value;
+
+        for(var i = 0; i < tableRecord.length; i++) {
+          if(tableRecord[i].value['隠しパラメータ'].value == "true") {
+            tableRecord[i].value['入力方法'].value = "手入力";
+            tableRecord[i].value['隠しパラメータ'].value = "";
+          }
+        }
+        kintone.app.record.set(rec);
+        return;
+      }
+      swal({
+        title: '経路を選択してください',
+        input: 'radio',
+        inputOptions: inputOptions,
+        inputValidator: function(value) {
+          return !value && "経路を選択してください。"
         }
       }).then(function (result) {
-        if (!depStation.code || !arrStation.code) {
-
+        if(!result.value){
           // テーブル値の更新
           var rec = kintone.app.record.get();
           var tableRecord = rec.record['明細'].value;
@@ -130,49 +150,25 @@ jQuery(function($) {
 
         }
         swal({
-          title: '経路を選択してください',
-          input: 'radio',
-          inputOptions: inputOptions,
-          inputValidator: function(value) {
-            return !value && "経路を選択してください。"
-          }
-        }).then(function (result) {
-          if(!result.value){
-            // テーブル値の更新
-            var rec = kintone.app.record.get();
-            var tableRecord = rec.record['明細'].value;
-
-            for(var i = 0; i < tableRecord.length; i++) {
-              if(tableRecord[i].value['隠しパラメータ'].value == "true") {
-                tableRecord[i].value['入力方法'].value = "手入力";
-                tableRecord[i].value['隠しパラメータ'].value = "";
-              }
-            }
-            kintone.app.record.set(rec);
-            return;
-
-          }
-          swal({
-            title: '受け付けました！',
-            type: "success"
-          })
-
-          // テーブル値の更新
-          var rec = kintone.app.record.get();
-          var tableRecord = rec.record['明細'].value;
-
-          for(var i = 0; i < tableRecord.length; i++) {
-            if(tableRecord[i].value['隠しパラメータ'].value == "true") {
-              tableRecord[i].value['経路'].value = selectRoutes[result.value].route;
-              tableRecord[i].value['金額'].value = selectRoutes[result.value].price;
-              tableRecord[i].value['隠しパラメータ'].value = "";
-              tableRecord[i].value['経路'].disabled = true;
-              tableRecord[i].value['金額'].disabled = true;
-            }
-          }
-          kintone.app.record.set(rec);
+          title: '受け付けました！',
+          type: "success"
         })
+
+        // テーブル値の更新
+        var rec = kintone.app.record.get();
+        var tableRecord = rec.record['明細'].value;
+
+        for(var i = 0; i < tableRecord.length; i++) {
+          if(tableRecord[i].value['隠しパラメータ'].value == "true") {
+            tableRecord[i].value['経路'].value = selectRoutes[result.value].route;
+            tableRecord[i].value['金額'].value = selectRoutes[result.value].price;
+            tableRecord[i].value['隠しパラメータ'].value = "";
+            tableRecord[i].value['経路'].disabled = true;
+            tableRecord[i].value['金額'].disabled = true;
+          }
+        }
+        kintone.app.record.set(rec);
       })
-    // }
+    })
   });
 });
